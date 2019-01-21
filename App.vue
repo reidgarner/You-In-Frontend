9a34549a06491bf90ff86fd2300a5c72a04cf0a5
reidgarner <reidbgarner@gmail.com>
2019-01-21 12:48:01 -0700

    first commit. built first draft sturcture of login and signup pages and integrated vue-native-router. Virtually no styling has been done yet.

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <app-navigation></app-navigation>
+</template>
+
+<script>
+import { StackNavigator } from "vue-native-router";
+import MainScreen from "./Main";
+import LoginScreen from "./Login";
+import SignupScreen from "./Signup";
+import GuestSUScreen from "./GuestSU";
+
+const AppNavigation = StackNavigator(
+  {
+    Main: MainScreen,
+    Login: LoginScreen,
+    Signup: SignupScreen,
+    GuestSU: GuestSUScreen
+  },
+  {
+    initialRouteName: 'Main',
+  }
+);
+export default {
+    components: { AppNavigation }
+}
+</script>
---
+++ Main.vue
@@ -0,0 +1,88 @@
+<template>
+    <view class="container">
+        <text class="text-color-primary">You In?</text>
+        <touchable-opacity class="button-1" :on-press="handleLogin">
+            <text class="button-text">Login</text>
+        </touchable-opacity>
+        <touchable-opacity class="button-2" :on-press="handleSignup">
+            <text class="button-text">Sign Up</text>
+        </touchable-opacity>
+        </view>
+</template>
+
+<script>
+import { Font } from 'expo';
+
+export default {
+    props: {
+        navigation: {
+            type: Object
+        }
+    },
+
+    data() {
+        return {
+            isLoaded: false
+    }
+    },
+
+    async mounted() {
+        await Expo.Font.loadAsync({
+            'Reenie Beanie': require('./assets/fonts/ReenieBeanie.ttf')
+        })
+    this.isLoaded = true;
+    },
+
+    methods: {
+        handleLogin() {
+            this.navigation.navigate("Login");
+        },
+        handleSignup() {
+            this.navigation.navigate("Signup");
+        }
+    }
+}
+</script>
+
+<style>
+.container {
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+}
+.text-color-primary {
+    color: black;
+    font-size: 50;
+    position: relative; bottom: 150;
+    font-family: Courier;
+}
+
+.button-1 {
+    background-color: rgb(67, 67, 67);
+    width: 200;
+    height: 50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10;
+    position: relative; top: 50;
+}
+
+.button-2 {
+    background-color: rgb(67, 67, 67);
+    width: 200;
+    height: 50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10;
+    position: relative; top: 80;
+}
+
+.button-text {
+    color: rgb(159, 159, 159);
+    font-size: 40;
+    font-family: Courier;
+}
+</style>
