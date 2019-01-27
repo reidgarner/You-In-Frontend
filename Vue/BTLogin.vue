d44d005f73ba68cd20484874d9b190d6238d7e97
reidgarner <reidbgarner@gmail.com>
2019-01-26 23:49:39 -0700

    styled bartender login page but 'Bartender' tag still needs styling

--- Vue/BTLogin.vue
+++ Vue/BTLogin.vue
@@ -1,18 +1,25 @@
 <template>
     <view class="container">
-        <text class="title">You In?</text>
-        <text class="As-a">Bartender</text>
-        <view class="input-container">
-            <text-input class="input" auto-capitalize="none" v-model="email" placeholder="Email"/>
-            <text-input class="input" auto-capitalize="none" v-model="password" placeholder="Password"/>
-        </view>
-        <touchable-opacity class="button" :on-press="handleYourGuests">
-            <text class="button-text">Login</text>
-        </touchable-opacity>
+        <image-background :source="bartenderImg" class="imageContainer">
+            <image :source="logo" class="logo"></image>
+            <image :source="logo2" class="logo2"></image>
+            <text class="As-a">Bartender</text>
+            <view class="input-container">
+                <text-input class="input" auto-capitalize="none" v-model="email" placeholder="Email"/>
+                <text-input class="input" auto-capitalize="none" v-model="password" placeholder="Password"/>
+            </view>
+            <touchable-opacity class="button" :on-press="handleYourGuests">
+                <text class="button-text">Login</text>
+            </touchable-opacity>
+        </image-background>
     </view>
 </template>
 
 <script>
+import bartenderImg from "/Users/reidgarner/Galv-Projects/CAPSTONE/You-In-Frontend/assets/Bartender3-Background.png";
+import logo from "/Users/reidgarner/Galv-Projects/CAPSTONE/You-In-Frontend/assets/red_cream_ud_logo_transparent.png";
+import logo2 from "/Users/reidgarner/Galv-Projects/CAPSTONE/You-In-Frontend/assets/YouIn_transparent.png";
+
 export default {
     props: {
         navigation: {
@@ -23,7 +30,10 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            logo: logo,
+            logo2: logo2,
+            bartenderImg: bartenderImg
     }
     },
 
@@ -42,34 +52,52 @@ export default {
     justify-content: center;
     flex: 1;
 }
-.title {
-    color: black;
-    font-size: 50;
-    position: relative; bottom: 150;
-    font-family: Courier;
+
+.imageContainer {
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    width: 130%;
+    position: relative; left: 46;
+}
+
+.logo {
+    height: 50%;
+    width: 50%;
+    position: relative; top: 175; right: 45;
+    opacity: .78;
+}
+
+.logo2 {
+    height: 54%;
+    width: 50%;
+    position: relative; bottom: 169; right: 45;
+    opacity: 1;
 }
 
 .button {
-    background-color: rgb(67, 67, 67);
-    width: 250;
-    height: 100;
+    background-color: #8D1B33;
+    width: 170;
+    height: 60;
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: 10;
-    position: relative; top: 80;
+    opacity: .85;
+    position: relative; bottom: 235; right: 47;
 }
 
 .button-text {
-    color: rgb(159, 159, 159);
-    font-size: 40;
-    font-family: Courier;
+    color: #F7C7AA;
+    font-size: 30;
+    font-family: Helvetica;
 }
 
 .As-a {
     color: black;
     font-size: 40;
-    font-family: Courier;
+    font-family: Helvetica;
+    position: relative; bottom: 250; right: 47;
 }
 
 .input-container {
@@ -77,7 +105,8 @@ export default {
     justify-content: space-evenly;
     height: 200;
     width: 300;
-
+    position: relative; bottom: 250; right: 47;
+    opacity: .85;
 }
 
 .input {
@@ -86,5 +115,6 @@ export default {
     border-radius: 10; 
     border-color: gray; 
     border-width: 1;
+    background-color: whitesmoke;
 }
 </style>
